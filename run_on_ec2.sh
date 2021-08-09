89c56dbf25217d994d67d85b24bf3b108aabf255
JoxFox <jingran.yu1988@gmail.com>
2021-08-09 15:57:30 +1000

    update the user data in launch template Signed-off-by: JoxFox <jingran.yu1988@gmail.com>

--- run_on_ec2.sh
+++ run_on_ec2.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-yum update
+yum update -y
 yum install ec2-instance-connect -y
-yum install apache2 -y
+yum install httpd -y
+service httpd start
+chkconfig httpd on
 
-ifconfig
-ip addr
-netstat -lnpt
-service sshd status
-pwd
-ls -l
-git clone 
+cd /var/www/html
+
+yum install git -y
+
+git clone https://github.com/joleneyu/i-am-jolene.git
+
+cd i-am-jolene/
+
+pip3 install pelican
+
+pip3 install markdown
+
+cd themes
+
+git clone https://github.com/joleneyu/pelican-alchemy.git
+
+cd ..
+
+make publish-elb
+
+cp -a ./docs/. /var/www/html
